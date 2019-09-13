806f3f521ef0478cd788994f77ca914bbd3a2265
Srikara S B <srikara.sb@robosoftin.com>
2019-09-13 16:09:51 +0530

    Smart India - CG Project

---
+++ Smart_India_bySBS_SGN/Smart_India_bySBS_SGN/sri.h
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+#include<Windows.h>
+#include<MMSystem.h>
+#include<dos.h>
+#include "glut.h"
+#include<time.h>
+#include<stdio.h>
+#include <string.h>
+#include<stdlib.h>
+#include<dos.h>
+void drawhouse();
+void cart123();
+//void rect(float,float,float,float);
+void delay();
+void drawroad(int);
+void stone();
+void gadde();
+void betta();
+void kamba();
+void sabka_saath();
+void cart();
+void tree();
+void rectd(float,float,float,float,float,float,float,float);
+//void tree1();
+//void tree2();
+void rect(float,float,float,float,float,float,float,float);
+void rect1(float,float,float,float,float,float,float,float,int);
+void modi();
+void initSecondWindow();
+void initdrawhouse();
+void drawhouse123();
+void cart1();
+void createNewWindow();
+void createNewWindow2();
+void createNewWindow3();
+void createNewwindow4();
+void drawflag();
+void callwindow();
+void initthirdWindow();
+void initforthwindow();
+void building();
+void school();
+void rocket();
+int n,p;
+float i=-30.0,m=-20.0,x=0.0,y=0.0;
+float firstWindow,secondWindow,third;
+float j=0.0,flagu=1,flagux=0;
+char *str1="SRINIVAS INSTITUTE OF TECHNOLOGY";
+char *str2="VALACHIL,MANGALURU-574143";
+char *str3="DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING";
+char *str4="COMPUTER GRAPHICS AND VISUALIZATION";
+char *str5="MINI PROJECT";
+char *str6="ON";
+char *str7="SMART INDIA";
+char *str8="TEAM MEMBERS:";
+char *str10="SRIKARA S B 4SN14CS100";
+char *str9="SHRISHA G N 4SN14CS097";
+char *str11="DEPT OF CSE,'B' SECTION";
+char *str12="PROJECT GUIDED BY:";
+char *str13="Mr.RAJESH D S";
+char *str14="DEPT OF CSE";
+char *str15="SIT,MANGALORE";
+char *str16="press n to continue.....";
